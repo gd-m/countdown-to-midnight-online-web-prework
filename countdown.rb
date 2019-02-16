0185d91820e7bf78f4fe8eda1c3a3ca50779ba7a
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-02-16 17:12:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,6 +11,15 @@ def countdown(count)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
+def countdown_with_sleep(counter)
+  n = 0
+  z = counter
+  while n < z
+    print "#{counter} SECOND(S)!\n"
+    counter -= 1
+    n += 1
+    sleep 1
+  end
+  return "HAPPY NEW YEAR!"
 
 end
