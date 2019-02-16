eea1866d23b8ca599bd3d101d69552d3c24f2b7e
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-02-16 17:01:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,9 @@
 
 def countdown(count)
   while count <= 0
-    phrase = "#{count} SECOND(S)!"
+    print "#{count} SECOND(S)!"
     count -= 1
-    puts "#{phrase}"
+  
   end
   puts "HAPPY NEW YEAR!"
 end
